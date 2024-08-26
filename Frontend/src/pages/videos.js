89b390a20a240de9e466914976aaf8609a64244d
Jabari Holloway <jabari@jabariholloway.com>
2024-08-26 14:47:01 -0500

    Add basic form

--- Frontend/src/pages/videos.js
+++ Frontend/src/pages/videos.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button, Form, Input } from "antd";
+import Modal from "../components/modal";
 
 const testData = [
   {
@@ -94,23 +96,69 @@ const testData = [
 ];
 
 function Videos() {
-
-
+  const [showForm, setShowForm] = useState(false);
 
   return (
     <div className="h-screen w-full bg-black">
       <div className="w-10/12 pt-8 mx-auto">
-        <h1 className="text-white text-5xl">YouTube Videos</h1>
+        <div className="flex justify-between">
+          <h1 className="text-white text-5xl">YouTube Videos</h1>
+          <Button onClick={() => setShowForm(true)}>Create</Button>
+        </div>
         <div className="mt-5 grid grid-cols-5 gap-4 justify-between">
           {testData.map((video) => (
             <div>
-                <img className="pt-0" src={video.thumbnail} />
-                <h3 className="text-white text-xl mb-0 pb-0">{video.name}</h3>
-                <h4 className="text-zinc-300">{video.artist}</h4>
+              <img className="pt-0" src={video.thumbnail} />
+              <h3 className="text-white text-xl mb-0 pb-0">{video.name}</h3>
+              <h4 className="text-zinc-300">{video.artist}</h4>
             </div>
           ))}
         </div>
       </div>
+      {showForm && (<Modal title={"Add YouTube Video"} onClose={() => setShowForm(false)}>
+        <Form name="basic"
+          labelCol={{ span: 8}}
+          wrapperCol={{ span: 16}}
+          style={{maxWidth: 600}}
+          >
+            <Form.Item
+              label="Name"
+              name="name"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input the video name',
+                },
+              ]}
+              >
+                <Input />
+            </Form.Item>
+            <Form.Item
+              label="Artist"
+              name="artist"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input the video artist',
+                },
+              ]}
+              >
+                <Input />
+            </Form.Item>
+            <Form.Item
+              label="Url"
+              name="url"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input the video url',
+                },
+              ]}
+              >
+                <Input />
+            </Form.Item>
+        </Form>
+      </Modal>)}
     </div>
   );
 }
