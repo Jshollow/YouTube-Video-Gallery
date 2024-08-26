8d4d46e0dc3383f8c62b14449d6268ba59d6714c
Jabari Holloway <jabari@jabariholloway.com>
2024-08-26 13:31:41 -0500

    Add video route

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Videos from "./pages/videos";
+import Video from "./pages/video";
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
       <Route path="/" element={<Videos />} />
+      <Route path="/video" element={<Video />} />
     </Routes>
     </BrowserRouter>
   );
