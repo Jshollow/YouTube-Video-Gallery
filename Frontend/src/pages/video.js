bee5b048ce03355cc2fadf25b47bd8568f80ee64
Jabari Holloway <jabari@jabariholloway.com>
2024-08-26 13:31:31 -0500

    Add Video page

---
+++ Frontend/src/pages/video.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Video() {
+    // get video id from route
+    return (
+      <div>
+            <h1>Video Details</h1>
+      </div>
+    );
+  }
+  
+  export default Video;
